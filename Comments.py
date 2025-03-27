4062dd2c1bb2bff6b5991a2d06cd6ff81d415d49
Chadwin Meko <chad.meks@gmail.com>
2025-03-27 11:38:48 +0200

    File changes were not saved in code editor

---
+++ Comments.py
@@ -0,0 +1,21 @@
+#Below is the code to display Hello World
+
+print("Hello World")
+
+#The below section will comment out multiple lines of code
+
+print("Chad")
+
+"""""
+print("This")
+print("Should")
+print("be")
+print("commented")
+print("out")
+
+"""""
+
+print("This is successful")
+
+
+#If there are only one set of """ then it comments everything out after those inverted comma's
---
+++ Python readme.md
@@ -0,0 +1,19 @@
+*Comments using python code*
+
+It allows us to describe what each part of the code does.
+
+
+-We use a # to specify Comments
+
+#This is a comment 
+
+
+-Make use of multi-line comments to comment out several lines of code:
+
+"""
+Line of code
+to
+comment
+out
+
+"""
