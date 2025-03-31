98dfa48c032f8d354c7ba93b53b5dafda4edebd0
Chadwin Meko <chad.meks@gmail.com>
2025-03-31 12:11:48 +0200

    If example

---
+++ IFPractice.py
@@ -0,0 +1,11 @@
+#Coding how a traffic light works
+
+light=input("Which color is the light indicating?")
+
+if light=="Green":
+    print("Please proceed")
+elif light=="Yellow":
+    print("Please proceed with caution")
+else:
+    print("Please stop")
+    
